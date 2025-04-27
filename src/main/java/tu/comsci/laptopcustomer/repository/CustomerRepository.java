acc4601b0aac40be56015e1ee2760a68b5f09239
Teerapat6509650468 <teerapat.kir@dome.tu.ac.th>
2025-04-28 01:47:59 +0700

    Revert "add customer service side"
    This reverts commit 4e79f8a034880c80783d6b9447d6896c8a2c8da5.

--- src/main/java/tu/comsci/laptopcustomer/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package tu.comsci.laptopcustomer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tu.comsci.laptopcustomer.model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
---
+++ src/main/java/tu/comsci/laptopcustomer/service/LaptopStoreClient.java
@@ -0,0 +1,41 @@
+package tu.comsci.laptopcustomer.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import tu.comsci.laptopcustomer.model.Laptop;
+
+import java.util.List;
+
+@Service
+public class LaptopStoreClient {
+
+    private final RestTemplate restTemplate;
+
+    public LaptopStoreClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public List<Laptop> getAllLaptops() {
+        String url = "http://localhost:8080/laptops"; 
+
+        ResponseEntity<List<Laptop>> response = restTemplate.exchange(
+                url, 
+                org.springframework.http.HttpMethod.GET, 
+                null, 
+                new org.springframework.core.ParameterizedTypeReference<List<Laptop>>() {}
+        );
+
+        return response.getBody();
+    }
+    
+    public boolean reserveLaptop(Long id) {
+        String url = "http://localhost:8080/laptops/" + id + "/reserve"; 
+
+        ResponseEntity<String> response = restTemplate.postForEntity(url, null, String.class);
+
+        return response.getStatusCode().is2xxSuccessful();
+    }
+
+}
