7d906a9f0e938ad02fdd84d6845cdc7125329bd0
Teerapat6509650468 <teerapat.kir@dome.tu.ac.th>
2025-04-27 16:41:12 +0700

    update to use lombok

--- src/main/java/tu/comsci/laptopadvisor/model/Laptop.java
+++ src/main/java/tu/comsci/laptopadvisor/model/Laptop.java
@@ -1,9 +1,17 @@
 package tu.comsci.laptopadvisor.model;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Laptop {
-
     private Long id;
 
     private String brand;
@@ -11,101 +19,17 @@ public class Laptop {
     private String processor;
     private int ram;
     private int storage;
-    private double price;    
+    private double price;
+
     
-    public Laptop() {
-
-	}
-
 	// Custom constructor without id
-    public Laptop(String brand, String model, String processor, int ram, int storage, double price) {
-        this.brand = brand;
-        this.model = model;
-        this.processor = processor;
-        this.ram = ram;
-        this.storage = storage;
-        this.price = price;
-    }
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getBrand() {
-		return brand;
-	}
-
-	public void setBrand(String brand) {
+	public Laptop(String brand, String model, String processor, int ram, int storage, double price) {
 		this.brand = brand;
-	}
-
-	public String getModel() {
-		return model;
-	}
-
-	public void setModel(String model) {
 		this.model = model;
-	}
-
-	public String getProcessor() {
-		return processor;
-	}
-
-	public void setProcessor(String processor) {
 		this.processor = processor;
-	}
-
-	public int getRam() {
-		return ram;
-	}
-
-	public void setRam(int ram) {
 		this.ram = ram;
-	}
-
-	public int getStorage() {
-		return storage;
-	}
-
-	public void setStorage(int storage) {
 		this.storage = storage;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
 		this.price = price;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(brand, id, model, price, processor, ram, storage);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Laptop other = (Laptop) obj;
-		return Objects.equals(brand, other.brand) && Objects.equals(id, other.id) && Objects.equals(model, other.model)
-				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price)
-				&& Objects.equals(processor, other.processor) && ram == other.ram && storage == other.storage;
-	}
-
-	@Override
-	public String toString() {
-		return "Laptop [id=" + id + ", brand=" + brand + ", model=" + model + ", processor=" + processor + ", ram="
-				+ ram + ", storage=" + storage + ", price=" + price + "]";
-	}
+	}	
         
 }
